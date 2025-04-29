3931d8ddf407a25d418fa536daeccc2e3827cae9
TrippyRahul <rahulrawat.trippybug@gmail.com>
2025-04-29 11:43:24 +0530

    INIT DEV-R

--- src/realtime/games/slots/base.slots.engine.ts
+++ src/realtime/games/slots/base.slots.engine.ts
@@ -4,7 +4,7 @@ import { SlotConfig, SlotSettings } from "./base.slots.type";
 export class SlotBaseEngine extends GameEngine {
     protected slotConfig: SlotConfig;
     protected settings: SlotSettings;
-
+///
     constructor(config: any) {
         super(config);
         this.slotConfig = config.content as SlotConfig;
